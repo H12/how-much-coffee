1f58ed041e4113572684b2c55b6d3b16d8ad02ba
Henry Firth <henrynf12@gmail.com>
2022-03-27 14:08:16 -0500

    Fix social cards

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,17 +18,17 @@ echo '<!DOCTYPE HTML>
 <meta name="apple-mobile-web-app-status-bar-style" content="default">
 <meta name="apple-mobile-web-app-title" content="Coffee Calculator">
 <meta charset="UTF-8">
-<meta name="description" content="A simple coffee calculator that determines the ideal brew ratio for your desired yield and strength. Custom formulas for both for pour over and french press!">
+<meta name="description" content="Find the ideal coffee-to-water ratio for pour over or french press! Simply select brew type and specify your desired yield and strength.">
 <meta name="theme-color" content="#e7decd">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta property="og:title" content="Simple Coffee Calculator">
+<meta property="og:title" content="Coffee Ratios - Simple Calculator | howmuch.coffee">
 <meta property="og:type" content="website">
 <meta property="og:url" content="https://howmuch.coffee">
 <meta property="og:image:url" content="http://howmuch.coffee/icons-1024.png">
-<meta property="og:image:secure_url" content="https://howmuch.coffee/icons-1024.png">
+<meta property="og:image:secure_url" content="https://howmuch.coffee/social-1200x630.png">
 <meta property="og:image:type" content="image/png">
-<meta property="og:image:width" content="1024">
-<meta property="og:image:height" content="1024">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
 <meta property="og:image:alt" content="Cartoon Coffee Beans">
 </head>
 <body style="overscroll-behavior-y: none;">
